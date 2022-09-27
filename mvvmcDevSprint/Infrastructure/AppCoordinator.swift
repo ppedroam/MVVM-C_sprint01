0b1be98acca10d9c982bd164c3cd9496a647fb8e
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2022-09-27 15:00:38 -0300

    fix: change architecter coordinator

--- mvvmcDevSprint/Infrastructure/AppCoordinator.swift
+++ mvvmcDevSprint/Infrastructure/AppCoordinator.swift
@@ -15,7 +15,7 @@ struct AppCoordinator {
     }
     
     func getRootViewController() -> UIViewController {
-        var architecter = Architecters.tatianaRico
+        var architecter = Architecters.elpidioGabriel
         switch architecter {
         case .raulRodrigo: return RRLoginViewController()
         case .henriqueAugusto: return HALoginViewController()
