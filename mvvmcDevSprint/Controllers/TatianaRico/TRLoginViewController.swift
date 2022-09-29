daa0fd21a315afc68f531c575a113285e414264e
Tatiana Rico <tatianarico1702@gmail.com>
2022-09-29 20:06:10 -0300

    criado viewModel, coordinator e aplicado SRP

--- mvvmcDevSprint/Controllers/TatianaRico/TRLoginViewController.swift
+++ mvvmcDevSprint/Controllers/TatianaRico/TRLoginViewController.swift
@@ -1,158 +1,51 @@
 import UIKit
 
 class TRLoginViewController: UIViewController {
-    
     @IBOutlet weak var heightLabelError: NSLayoutConstraint!
     @IBOutlet weak var errorLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var createAccountButton: UIButton!
-    
     @IBOutlet weak var showPasswordButton: UIButton!
-    var showPassword = true
-    var errorInLogin = false
-    
     @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
     var initialConstant: CGFloat = 0
     let defaultSpacing: CGFloat = 100
     var yVariation: CGFloat = 0
     var textFieldIsMoving = false
+    var showPassword = true
+    var errorInLogin = false
+    var viewModel = TRLoginViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         verifyLogin()
-
-        #if DEBUG
-        emailTextField.text = "mvvmc@devpass.com"
-        passwordTextField.text = "Abcde1"
-        #endif
-
+        
+        self.ifDebugPasswordMock()
         self.setupView()
         self.validateButton()
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardDidHideNotification, object: nil)
-        initialConstant = topConstraint.constant
-    }
-    
-    open override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
-    func verifyLogin() {
-        if let _ = UserDefaultsManager.UserInfos.shared.readSesion() {
-            let vc = UINavigationController(rootViewController: HomeViewController())
-            let scenes = UIApplication.shared.connectedScenes
-            let windowScene = scenes.first as? UIWindowScene
-            let window = windowScene?.windows.first
-            window?.rootViewController = vc
-            window?.makeKeyAndVisible()
-        }
+        self.configNotificationCenter()
     }
     
     @IBAction func loginButton(_ sender: Any) {
         didClickLogin()
     }
     
-    func didClickLogin() {
-        if !ConnectivityManager.shared.isConnected {
-            let alertController = UIAlertController(title: "Sem conexão", message: "Conecte-se à internet para tentar novamente", preferredStyle: .alert)
-            let actin = UIAlertAction(title: "Ok", style: .default)
-            alertController.addAction(actin)
-            present(alertController, animated: true)
-            return
-        }
-
-        showLoading()
-        let parameters: [String: String] = ["email": emailTextField.text!,
-                                            "password": passwordTextField.text!]
-        let endpoint = Endpoints.Auth.login
-        AF.request(endpoint, method: .get, parameters: parameters, headers: nil) { result in
-            DispatchQueue.main.async {
-                self.stopLoading()
-                switch result {
-                case .success(let data):
-                    let decoder = JSONDecoder()
-                    if let session = try? decoder.decode(Session.self, from: data) {
-                        let vc = UINavigationController(rootViewController: HomeViewController())
-                        let scenes = UIApplication.shared.connectedScenes
-                        let windowScene = scenes.first as? UIWindowScene
-                        let window = windowScene?.windows.first
-                        window?.rootViewController = vc
-                        window?.makeKeyAndVisible()
-                        UserDefaultsManager.UserInfos.shared.save(session: session, user: nil)
-                    } else {
-                        Globals.alertMessage(title: "Ops..", message: "Houve um problema, tente novamente mais tarde.", targetVC: self)
-                    }
-                case .failure:
-                    self.setErrorLogin("E-mail ou senha incorretos")
-                    Globals.alertMessage(title: "Ops..", message: "Houve um problema, tente novamente mais tarde.", targetVC: self)
-                }
-            }
-        }
-    }
-    
     @IBAction func showPassword(_ sender: Any) {
-        if(showPassword == true) {
-            passwordTextField.isSecureTextEntry = false
-            showPasswordButton.setImage(UIImage.init(systemName: "eye.slash")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        } else {
-            passwordTextField.isSecureTextEntry = true
-            showPasswordButton.setImage(UIImage.init(systemName: "eye")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        }
+        let imageName = showPassword ? "eye.slash" : "eye"
+        let image = UIImage.init(systemName: imageName)?.withRenderingMode(.alwaysTemplate)
+        showPasswordButton.setImage(image, for: .normal)
+        passwordTextField.isSecureTextEntry = showPassword
         showPassword = !showPassword
     }
     
     @IBAction func resetPasswordButton(_ sender: Any) {
-        let vc = TRResetPasswordViewController()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
+        self.viewModel.goToResetPassword()
     }
-    
     
     @IBAction func createAccountButton(_ sender: Any) {
-        let controller = TRCreateAccountViewController()
-        controller.modalPresentationStyle = .fullScreen
-        present(controller, animated: true)
-    }
-    
-    func setupView() {
-        heightLabelError.constant = 0
-        loginButton.layer.cornerRadius = loginButton.frame.height / 2
-        loginButton.backgroundColor = .blue
-        loginButton.setTitleColor(.white, for: .normal)
-        loginButton.isEnabled = true
-
-        showPasswordButton.tintColor = .lightGray
-
-        createAccountButton.layer.cornerRadius = createAccountButton.frame.height / 2
-        createAccountButton.layer.borderWidth = 1
-        createAccountButton.layer.borderColor = UIColor.blue.cgColor
-        createAccountButton.setTitleColor(.blue, for: .normal)
-        createAccountButton.backgroundColor = .white
-        
-        emailTextField.setDefaultColor()
-        passwordTextField.setDefaultColor()
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(didClickView))
-        view.addGestureRecognizer(gesture)
-        view.isUserInteractionEnabled = true
-        validateButton()
-        
-        emailTextField.returnKeyType = .next
-        passwordTextField.returnKeyType = .done
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
-    }
-
-    @objc
-    func didClickView() {
-        view.endEditing(true)
+        self.viewModel.goToCreatAccount()
     }
     
     //email
@@ -189,6 +82,101 @@ class TRLoginViewController: UIViewController {
         passwordTextField.setDefaultColor()
     }
     
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func ifDebugPasswordMock() {
+#if DEBUG
+        emailTextField.text = "mvvmc@devpass.com"
+        passwordTextField.text = "Abcde1"
+#endif
+    }
+    
+    func configNotificationCenter() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    func verifyLogin() {
+        self.viewModel.verifyLogin()
+    }
+    
+    func didClickLogin() {
+        let email = emailTextField.text ?? ""
+        let password = emailTextField.text ?? ""
+        
+        if !ConnectivityManager.shared.isConnected {
+            self.viewModel.alertConexao(title: "Sem conexão", message: "Conecte-se à internet para tentar novamente")
+        }
+        
+        showLoading()
+        self.viewModel.requestScreenLogin(email: email, password: password)
+    }
+    
+    func globalsAlerts(title: String, message: String) {
+        Globals.alertMessage(title: title, message: message, targetVC: self)
+    }
+    
+    func setupView() {
+        initialConstant = topConstraint.constant
+        showPasswordButton.tintColor = .lightGray
+        heightLabelError.constant = 0
+        
+        setupLoginBtn()
+        setupCreateAccount()
+        setupTextField()
+        gestureBtnDidClickView()
+        validateButton()
+    }
+    
+    private func gestureBtnDidClickView() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didClickView))
+        view.addGestureRecognizer(gesture)
+        view.isUserInteractionEnabled = true
+    }
+    
+    private func setupLoginBtn() {
+        loginButton.layer.cornerRadius = loginButton.frame.height / 2
+        loginButton.backgroundColor = .blue
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.isEnabled = true
+    }
+    
+    private func setupCreateAccount() {
+        createAccountButton.layer.cornerRadius = createAccountButton.frame.height / 2
+        createAccountButton.layer.borderWidth = 1
+        createAccountButton.layer.borderColor = UIColor.blue.cgColor
+        createAccountButton.setTitleColor(.blue, for: .normal)
+        createAccountButton.backgroundColor = .white
+    }
+    
+    private func setupTextField() {
+        emailTextField.setDefaultColor()
+        passwordTextField.setDefaultColor()
+        emailTextField.returnKeyType = .next
+        passwordTextField.returnKeyType = .done
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    @objc func didClickView() {
+        view.endEditing(true)
+    }
+    
+    func goToHome() {
+        let vc = UINavigationController(rootViewController: HomeViewController())
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+    
     func setErrorLogin(_ message: String) {
         errorInLogin = true
         heightLabelError.constant = 20
@@ -209,32 +197,15 @@ class TRLoginViewController: UIViewController {
     }
     
     func validateButton() {
-        if !emailTextField.text!.contains(".") ||
-            !emailTextField.text!.contains("@") ||
-            emailTextField.text!.count <= 5 {
-            disableButton()
-        } else {
-            if let atIndex = emailTextField.text!.firstIndex(of: "@") {
-                let substring = emailTextField.text![atIndex...]
-                if substring.contains(".") {
-                    enableButton()
-                } else {
-                    disableButton()
-                }
-            } else {
-                disableButton()
-            }
-        }
+        self.changeButtonStatus(color: .gray, isEnabled: false)
+        let email = emailTextField.text
+        
+        self.viewModel.isEmailValid(email: email ?? "") ? changeButtonStatus(color: .blue, isEnabled: true) : changeButtonStatus(color: .gray, isEnabled: false)
     }
     
-    func disableButton() {
-        loginButton.backgroundColor = .gray
-        loginButton.isEnabled = false
-    }
-    
-    func enableButton() {
-        loginButton.backgroundColor = .blue
-        loginButton.isEnabled = true
+    private func changeButtonStatus(color: UIColor, isEnabled: Bool) {
+        loginButton.backgroundColor = color
+        loginButton.isEnabled = isEnabled
     }
 }
 
@@ -253,19 +224,17 @@ extension TRLoginViewController: UITextFieldDelegate {
 //MARK: keyboard appearence manager
 
 extension TRLoginViewController {
-    
-    @objc
-    func keyboardWillShow(_ notification: Notification) {
+    @objc func keyboardWillShow(_ notification: Notification) {
         guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
               isNeededToMoveTextfield(keyboardOriginY: keyboardSize.minY) else {
-                  return
-              }
-
+            return
+        }
+        
         guard !textFieldIsMoving else { return }
         textFieldIsMoving = true
-
+        
         let currentConstraintValue = topConstraint.constant
-
+        
         UIView.animate(withDuration: 0.1, animations: {
             self.topConstraint.constant = currentConstraintValue - self.yVariation
             self.view.layoutIfNeeded()
@@ -274,12 +243,11 @@ extension TRLoginViewController {
         }
     }
     
-    @objc
-    func keyboardWillHide(_ notification: Notification) {
+    @objc func keyboardWillHide(_ notification: Notification) {
         if topConstraint.constant != initialConstant {
             guard !textFieldIsMoving else { return }
             textFieldIsMoving = true
-
+            
             UIView.animate(withDuration: 0.1, animations: {
                 self.topConstraint.constant = self.initialConstant
                 self.view.layoutIfNeeded()
@@ -294,8 +262,8 @@ extension TRLoginViewController {
               let activeTextField = textFields.first(where: {$0.isFirstResponder}),
               let activeTFWindowPosition = activeTextField.superview?.convert(activeTextField.frame, to: nil) else {
             return false
-              }
-
+        }
+        
         let textFieldHeight = activeTextField.frame.height
         let newOriginY = keyboardOriginY - textFieldHeight - defaultSpacing
         yVariation = activeTFWindowPosition.minY - newOriginY
